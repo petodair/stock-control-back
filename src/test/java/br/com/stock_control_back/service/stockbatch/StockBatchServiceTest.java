1ff227a7d03d1b1d504c5802876645895b955d98
Peterson Odair <petersomodair@gmail.com>
2026-01-20 17:09:28 -0300

    test: criando classe de teste do StockBatchService

---
+++ src/test/java/br/com/stock_control_back/service/stockbatch/StockBatchServiceTest.java
@@ -0,0 +1,10 @@
+package br.com.stock_control_back.service.stockbatch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StockBatchServiceTest {
+
+    @Autowired
+    IStockBatchService stockBatchService;
+
+}
