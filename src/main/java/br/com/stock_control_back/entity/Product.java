24372e5819b0f07310bc56ab4cb7e49abb41907a
Peterson Odair <petersomodair@gmail.com>
2026-01-19 23:25:09 -0300

    feat: criação e mapeamento da entidade Product

---
+++ src/main/java/br/com/stock_control_back/entity/Product.java
@@ -0,0 +1,32 @@
+package br.com.stock_control_back.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(precision = 18, scale = 2)
+    private BigDecimal price;
+}
