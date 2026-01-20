d85533bf705ca4cf19343bdb32cf16e7b41160f3
Peterson Odair <petersomodair@gmail.com>
2026-01-20 19:08:41 -0300

    feat: adicionado StockBatchReportDTO

---
+++ src/main/java/br/com/stock_control_back/dto/stockbatch/StockBatchReportDTO.java
@@ -0,0 +1,16 @@
+package br.com.stock_control_back.dto.stockbatch;
+
+import java.math.BigDecimal;
+
+public record StockBatchReportDTO(
+        String productName,
+        String productCode,
+        BigDecimal price,
+
+        String batchNumber,
+        String manufacturing,
+        String validity,
+        Integer quantity,
+        String location
+) {
+}
